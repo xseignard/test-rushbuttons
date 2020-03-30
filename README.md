09ddd29bbc7691372d7b856371ffbad48269c81b
xseignard <xavier.seignard@gmail.com>
2020-03-30 10:30:16 +0200

    Updated code and docs

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 ## Test setup
 
 - board: https://robotdyn.com/samd21-m0-mini.html
+- variant: https://github.com/sparkfun/Arduino_Boards/blob/master/sparkfun/samd/variants/SparkFun_SAMD_Mini/variant.cpp
 - TLC module: https://www.adafruit.com/product/1455
+
+## MCU pinout
+
+![pinout](./.github/images/pinout.png)
--- src/main.ino
+++ src/main.ino
@@ -1,15 +1,29 @@
 #include <Adafruit_TLC59711.h>
+#include <SPI.h>
+#include <wiring_private.h>
 
 // This one is working, e.g using default pins
 // data: 11
 // clock: 13
-Adafruit_TLC59711 tlc = Adafruit_TLC59711(1);
+// Adafruit_TLC59711 tlc = Adafruit_TLC59711(1);
 
 // The following is not working
-Adafruit_TLC59711 tlc = Adafruit_TLC59711(1, 5, 4);
+// bit-banging mode
+// Adafruit_TLC59711 tlc = Adafruit_TLC59711(1, 5, 4);
+
+// SERCOM SPI
+// SPIClass SPI (&PERIPH_SPI, PIN_SPI_MISO, PIN_SPI_SCK, PIN_SPI_MOSI, PAD_SPI_TX, PAD_SPI_RX);
+// not working...
+SPIClass SPI2(&sercom2, 3, 5, 4, SPI_PAD_0_SCK_3, SERCOM_RX_PAD_1);
+
+Adafruit_TLC59711 tlc = Adafruit_TLC59711(1, &SPI2);
 
 void setup() {
   SerialUSB.begin(9600);
+  SPI2.begin();
+  pinPeripheral(3, PIO_SERCOM_ALT);
+  pinPeripheral(4, PIO_SERCOM_ALT);
+  pinPeripheral(5, PIO_SERCOM);
   tlc.begin();
   tlc.write();
 }
