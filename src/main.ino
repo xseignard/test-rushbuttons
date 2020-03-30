72e85f8bd6c57796c9c5632df56c584e7db55984
xseignard <xavier.seignard@gmail.com>
2020-03-30 17:20:46 +0200

    begin spi before pinPeripheral and 2 TLCs

--- src/main.ino
+++ src/main.ino
@@ -15,26 +15,26 @@
 // not working...
 SPIClass SPI2(&sercom2, 3, 5, 4, SPI_PAD_0_SCK_3, SERCOM_RX_PAD_1);
 
-Adafruit_TLC59711 tlc = Adafruit_TLC59711(1, &SPI2);
+Adafruit_TLC59711 tlc = Adafruit_TLC59711(2, &SPI2);
 
 void setup() {
   SerialUSB.begin(9600);
+  tlc.begin();
   pinPeripheral(3, PIO_SERCOM_ALT);
   pinPeripheral(4, PIO_SERCOM_ALT);
   pinPeripheral(5, PIO_SERCOM);
-  tlc.begin();
   tlc.write();
 }
 
 void loop() {
   SerialUSB.println("White");
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 8; i++) {
     tlc.setLED(i, 65535, 65535, 65535);
   }
   tlc.write();
   delay(1000);
   SerialUSB.println("Black");
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 8; i++) {
     tlc.setLED(i, 0, 0, 0);
   }
   tlc.write();
